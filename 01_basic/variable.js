31d09f3ad7685501f3fb2cd1e1ba0c35ea0b4b5e
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-14 10:41:27 +0000

    container config done

---
+++ 01_basic/variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+// accountId =11
+
+
+let accountEmail = "tahapetkar2@gmail.com "
+var accountPassword = "1234556"
+accountCity = "Delhi"
+let accountState;
+
+accountEmail = "petkar2@gmail.com "
+accountPassword = "6554321"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
