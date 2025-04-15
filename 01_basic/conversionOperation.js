eada1206b6e5f873a791738fd44158ff2f0f236a
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-15 04:07:07 +0000

    container config done

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,59 @@
+// STRING
+// let score = "33abc" => NaN
+// console.log(typeof (score));
+// Convert Number to String 
+let someNumber = 34
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+
+
+// Number
+// let score = "33" => 33
+// let valueNumber = Number(score)
+// console.log(typeof (valueNumber));
+// console.log(valueNumber);
+
+
+
+// NULL
+// let score = null     
+//  null is a Object
+// console.log(typeof score);
+
+
+
+// UNDEFINED
+// let score = undefined
+// undefined is a undefined
+// console.log(typeof score);
+
+
+
+
+
+
+// true is a boolean value and give 1
+// false is a boolean value and give 0
+// "" in boolean give false
+// "Taha" in boolean give true
+
+// let score = false
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+// console.log(valueNumber);
+
+// Boolean
+
+// let logged = 1
+
+// let booleanLogged = Boolean(logged )
+// console.log(booleanLogged );
+
+
+
+
+
+
+
