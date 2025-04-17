9ad2c75dc4e9605645a24893c0b17855ca8353cf
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-17 16:54:05 +0000

    variable

--- 01_basic/variable.js
+++ 01_basic/variable.js
@@ -1,15 +1,18 @@
 const accountId = 12345
 // accountId =11
-
-
 let accountEmail = "tahapetkar2@gmail.com "
 var accountPassword = "1234556"
 accountCity = "Delhi"
 let accountState;
 
-accountEmail = "petkar2@gmail.com "
-accountPassword = "6554321"
+let accountEmail1 = "petkar2@gmail.com "
+const accountPasswordNew = "6554321"
 accountCity = "Mumbai"
 
-console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+accountEmail = "mohd@gmail.com"
+accountPassword = "5343452"
+accountCity = "Thane"
+
+// console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
