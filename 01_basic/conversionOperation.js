befcfc7742aece8de2fda1ca123b91314a2191e8
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-17 19:16:50 +0000

    coversion and operations

--- 01_basic/conversionOperation.js
+++ 01_basic/conversionOperation.js
@@ -2,10 +2,10 @@
 // let score = "33abc" => NaN
 // console.log(typeof (score));
 // Convert Number to String 
-let someNumber = 34
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber = 34
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
 
 
 
@@ -51,3 +51,78 @@ console.log(typeof stringNumber)
 // let booleanLogged = Boolean(logged )
 // console.log(booleanLogged );
 
+// number
+// let score=35;      //Number
+
+// let score = "35"    //String     "35"=>35
+// let score = "36 taha"            "36taha" =>Nan 
+
+// let score =null          // null Value 
+
+// let score =undefined    //undefined Value 
+
+//let score = false         // boolean value   true=>1;false=>0
+
+// let logged =""                          //"" =>false;"taha" =>true; " "=>true
+// let loggedBoolean = Boolean(logged)
+// console.log(loggedBoolean);
+
+
+
+// console.log(score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// String
+
+    // let number = 23
+    // let numberString = String(number)
+    // console.log(typeof numberString); //33=>String
+    
+    // console.log(numberString); //=>33
+
+// ******************************** OPERATIONS ***************************************
+
+//Negative Value Convert
+
+    // let value =5
+    // let valueNeg = -value
+    // console.log(valueNeg); //5=>-5
+    
+//  Operations 1
+    // let str1 = "Hello"
+    // let str2 = "Taha"
+    // let str3 =str1+str2
+    // console.log(str3); =>HelloTaha
+
+    // Operation 2
+    // console.log("1" + 2);
+    // console.log(1 + "2");
+    // console.log("1" + 2 + "2");
+    // console.log(1 + 2 +"2"); //=>32
+    // console.log(+true); //=>1
+    // console.log(+""); //=>0
+    // console.log(+false);//=>0
+
+
+    // Operations 3
+    // let game=100
+    // // game++
+    // ++game
+    // console.log(game); //=>101
+
+    // postfix
+    // let a=3
+    // const b = a++
+    // console.log(`a:${a},b:${b}`); //postfix =>a=4;y=3
+    
+    // Prefix
+    // let x=5
+    // const y= ++x
+    // console.log(`x:${x},y:${y}`); //Prefix =>x:6,y:6
+    
+    
+    
