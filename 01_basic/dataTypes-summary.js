ce744ff28bc6baa77927e67d119fa5c293a239e9
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-18 10:58:39 +0000

    dataTypes Summary

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,38 @@
+// PRIMATIVE DATATYPE
+//7 TYPES 
+
+// String ,Boolean, Null, Number, Undefined, Symbol, BigInt
+const name = "321"
+const Name = "Taha"
+
+const LoggedIn = false
+
+const Number = 23
+
+let accountCity; // const keyword undefined me nahi lagta hai  only scalar on(bool,int, float & string)
+
+const userEmail = null
+
+ const accountId = Symbol('123')
+ const otherId = Symbol('123')
+
+ console.log(accountId === otherId);
+ 
+
+//  Non PRIMATIVE 
+//  In Non Primative rumtime we will get Object only
+
+//Array, Object, Function
+ const hero=["Taha", "Mohd", "Petkar"]
+
+//  Object 
+ const obj={
+   namme :"taha",
+    age:23,
+ }
+
+//  Function
+ const myFunction =function(){
+    console.log("Hello World");
+    
+ }
