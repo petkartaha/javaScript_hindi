7cae9d4488621570e5f3656b8c5acfdab76e9375
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-19 18:48:58 +0000

    objects

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,39 @@
+// singleton
+
+
+const symbol=Symbol("mySym")
+// Object literals
+
+const jsUser={
+    "full name": "taha petkar",
+    name:"taha",
+    email:"taha@gmail.com",
+    age: 23,
+    city:"Delhi",
+    [symbol]: "myKey",
+}
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full name"]);
+// console.log(typeof jsUser["symbol"]);
+
+// Object ko change
+
+jsUser.email = "petkar@gmail.com"
+
+
+// Object.freeze (jsUser)
+jsUser.email = ("mohd@gmail.com")
+// console.log(jsUser);
+
+// Functions ko Object me variable ke tarha treat ki jiye 
+
+jsUser.greeting = function(){
+    console.log("Hello JS User");
+}
+console.log(jsUser.greeting());
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello Taha,${this.name}`); //=>`` isko bolte hai String Interpolation
+    }
+console.log(jsUser.greetingTwo());
