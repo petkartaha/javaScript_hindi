e0ad3647321e2c345c69a26ba769a9eb0d8c4122
Petkar Taha <petkar.mohdtaha@gmail.com>
2025-04-25 18:30:00 +0000

    01-Function

---
+++ 03_basic/01_function.js
@@ -0,0 +1,64 @@
+function myName(){       //ye pure ko defination bolte hai
+    console.log("T");     // myName() //Execution jaise ()ye lag gaya to usko excution bolte hai
+                        // myName  //isse refference bolte hai 
+                        // () Syntax
+                        // Function is a Keyword
+                        // myName is a Function Name 
+    console.log("A");   
+    console.log("H");
+    console.log("A");
+}
+//  myName()
+
+//  Sum of NUMBER
+
+// Phala tarika
+// function addNumber(num1,num2){
+//     console.log(num1+num2);
+    
+// }
+
+// let result = addNumber(3,9)
+// console.log("result:",result);
+// return result;                  //return is key word
+
+
+// Second Tarika
+
+// function addNumber2(num1,num2){
+//     let result = num1+ num2;
+//     return result;
+// }
+// let result = addNumber2(8,7)
+// console.log("Result:", result);
+
+// Third Tarika
+
+// function addNumber3(num1,num2){
+//     return num1 + num2;
+// }
+// let result = addNumber3(4,6)
+// console.log("result:",result);
+
+// Fouth Tarika
+
+// function userName(name){
+//     if(!name ){
+//       console.log("please enter a name");
+//         return // ===> Agar
+//     }
+//     return `${name} Login your username `
+// }
+// console.log(userName("Taha"))
+
+// Fivth Tarika
+
+function userName(name = "Petkar"){   // ==>Petkar is default Value given.
+    if(name === undefined){
+        console.log("please enter a username.")
+    }
+    let result = name
+    return result;
+}
+let result = userName()
+console.log("Just login a Username:",result)
